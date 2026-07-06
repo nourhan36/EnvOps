171307508d8c48606881ec795283229a9c4f0aaa
Alaa magdy <alaamagdy3008@gmail.com>
2026-07-06 23:55:51 +0600

    added vpc module usage in main.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,5 +1,34 @@
 variable "region" {
-  description = "AWS Region"
   type        = string
-  default     = "us-east-1"
+  description = "AWS region to deploy resources"
+  default = "us-east-1"
 }
+
+variable "project_name" {
+  description = "Project name for tagging and resource naming"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+
+variable "tags" {
+  type = map(string)
+}
+
+
