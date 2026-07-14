ef19cb8f49c796512bf7a8606d50697a4c5a238d
Alaa magdy <alaamagdy3008@gmail.com>
2026-07-14 22:44:10 +0600

    add cron job to check expired sandboxes every 1 min

--- apps/backend/src/workers/eviction.worker.ts
+++ apps/backend/src/workers/eviction.worker.ts
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 import { runEvictionCycle } from "../services/eviction.service";
-
+// will be updated to delete expired sandboxes and their associated resources in the future
 export function startEvictionWorker() {
 
     console.log("Eviction Worker Started");
