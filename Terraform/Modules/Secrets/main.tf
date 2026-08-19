636973f2c61bfe5dcdcd8b5ebd788bff3202a32a
Alaa magdy <alaamagdy3008@gmail.com>
2026-08-19 16:45:32 +0600

    install secret controller using helm by terraform for automation

--- Terraform/Modules/Secrets/main.tf
+++ Terraform/Modules/Secrets/main.tf
@@ -1,3 +1,28 @@
+resource "helm_release" "external_secrets" {
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  namespace  = var.namespace
+  version    = var.helm_chart_version
+
+  create_namespace = true
+  atomic           = true
+  wait             = true
+  timeout          = 600
+
+  values = [
+    yamlencode({
+      serviceAccount = {
+        create = true
+        name   = var.service_account_name
+        annotations = {
+          "eks.amazonaws.com/role-arn" = var.irsa_role_arn
+        }
+      }
+      installCRDs = true
+    })
+  ]
+}
 data "aws_iam_policy_document" "eso_secrets_policy" {
   statement {
     effect = "Allow"
--- Terraform/Modules/Secrets/variables.tf
+++ Terraform/Modules/Secrets/variables.tf
@@ -1,3 +1,7 @@
 variable "region" { type = string }
 variable "account_id" { type = string }
 variable "policy_name" { type = string }
+variable "namespace" { type = string }
+variable "service_account_name" { type = string }
+variable "irsa_role_arn" { type = string }
+variable "helm_chart_version" { type = string }
