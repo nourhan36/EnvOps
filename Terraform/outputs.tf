7c63f61a2627d125f9ff7e4ea000c6410686e4b0
Alaa Magdy <104599398+alaamagdy20211@users.noreply.github.com>
2026-08-20 22:30:06 +0600

    Terraform/testing (#27)
    * add efs module to be used as a persistent volume for jenkins
    
    * install secret controller using helm by terraform for automation
    
    * add iam module to solve the problem of circular dependency in secrets module
    
    * update in sercet module
    
    * test all modules
    
    * testing all modules
    
    * test pipeline
    
    * test
    
    * test pipeline
    
    * Update workflow triggers in main.yml
    
    Removed push trigger for specific branch and paths.
    
    * test infra

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "external_secrets_role_arn" {
-  description = "IAM role ARN used by External Secrets Operator."
-  value       = module.irsa.role_arn
-}
+# output "external_secrets_role_arn" {
+#   description = "IAM role ARN used by External Secrets Operator."
+#   value       = module.irsa.role_arn
+# }
 
-output "external_secrets_role_name" {
-  description = "IAM role name used by External Secrets Operator."
-  value       = module.irsa.role_name
-}
+# output "external_secrets_role_name" {
+#   description = "IAM role name used by External Secrets Operator."
+#   value       = module.irsa.role_name
+# }
