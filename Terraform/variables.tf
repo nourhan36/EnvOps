f15be3ae0b9b2cdefb7f11be9cf5a0a602cfbd0f
Alaa magdy <alaamagdy3008@gmail.com>
2026-08-24 06:37:36 +0600

    restore clean Terraform variables

---
+++ Terraform/variables.tf
@@ -0,0 +1,77 @@
+variable "region" {
+  type        = string
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name for tagging and resource naming"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "repositories" {
+  description = "List of ECR repositories to create"
+  type        = list(string)
+
+  default = [
+    "frontend",
+    "backend"
+  ]
+}
+
+variable "git_repo_url" {
+  description = "Git repository containing the EnvOps project"
+  type        = string
+}
+
+variable "git_branch" {
+  description = "Git branch used by Argo CD and Image Updater"
+  type        = string
+  default     = "main"
+}
+
+
+
+
+variable "domain_name" {
+  description = "Public domain name for EnvOps"
+  type        = string
+}
+
+variable "acme_email" {
+  description = "Email address used by Let's Encrypt"
+  type        = string
+}
+variable "git_token" {
+  description = "ACME server URL for Let's Encrypt"
+  type        = string
+
+}
+variable "git_username" {
+  description = "GitHub username for Argo CD/Image Updater"
+  type        = string
+  sensitive   = true
+  default     = "nourhan36"
+}
